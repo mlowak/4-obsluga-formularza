3d41b2a27b29f34f63811e0356fc5580a891d11a
mlowak <67906604+mlowak@users.noreply.github.com>
2020-08-26 20:01:39 +0200

    poprawka literówki

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 const wyslijFormularz = (event) => {
-    event.prevontDefault();
+    event.preventDefault();
 
     let firstName = document.querySelector('[name="fname"]');
     let lastName = document.querySelector('[name="lname"]');
 
-    console.log(`Imię: ${firstName.value}, nazwisko ${lastName.value}`);
+    console.log(`Imię: ${firstName.value}, Nazwisko: ${lastName.value}`);
 
     // można też stosować pętlę, jeśli formularz posiada wiele pól
 }
